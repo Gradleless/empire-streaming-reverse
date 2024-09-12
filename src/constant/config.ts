0d2bf7ef1e6f5e52ffa566e7deaaf7c971852747
Lyciaa <57102194+Gradleless@users.noreply.github.com>
2024-09-12 16:12:06 +0200

    ah anime de con

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -3,5 +3,5 @@ export const siteConfig = {
   description:
     'Un simple bypasser pour le site empire-streaming.life, toutes les vidéos, sans publicités.',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://anime-streaming-life-bypasser.vercel.app',
+  url: 'https://empire-streaming-life-bypasser.vercel.app',
 };
