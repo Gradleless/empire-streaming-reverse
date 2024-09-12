802e100f4b0aacb926577435947d2b5f32900f52
Lyciaa <57102194+Gradleless@users.noreply.github.com>
2024-09-12 16:03:47 +0200

    oopsie empire pas anime mgl

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
-  title: 'anime-streaming.life bypasser',
+  title: 'empire-streaming.life bypasser',
   description:
-    'Un simple bypasser pour le site anime-streaming.life, toutes les vidéos, sans publicités.',
+    'Un simple bypasser pour le site empire-streaming.life, toutes les vidéos, sans publicités.',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
   url: 'https://anime-streaming-life-bypasser.vercel.app',
 };
